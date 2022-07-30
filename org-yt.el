05ac11a8a16c6510c2ff2bb3720221ea00cdeaba
Nopanun Laochunhanun <nopanun@pm.me>
2022-07-30 13:23:38 +0700

    tweak: don't put description, it may use as overlay

--- org-yt.el
+++ org-yt.el
@@ -184,9 +184,7 @@ with one of the formats [[PROTOCOL:LINK]] or [[PROTOCOL:LINK][DESCRIPTION]] are 
                               (overlay-get (cdr old) 'display)))
 		       (funcall (cdr image-data-link) protocol link description)))
              (when image-data
-               (let ((ol (org-image-update-overlay image-data el t t)))
-                 (when (and ol description)
-                   (overlay-put ol 'after-string description)))))))))))
+               (org-image-update-overlay image-data el t t)))))))))
 
 (advice-add #'org-display-inline-images :after #'org-display-user-inline-images)
 
